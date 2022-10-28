32bd96fb6c7f3dd6bdf4676168afdb98cbaa21aa
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-10-28 22:18:49 +0900

    Add publishAuthenticationSuccess

--- src/main/java/demo/MyAuthenticationEventPublisher.java
+++ src/main/java/demo/MyAuthenticationEventPublisher.java
@@ -4,6 +4,8 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.client.authentication.OAuth2LoginAuthenticationToken;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 
 public class MyAuthenticationEventPublisher extends DefaultAuthenticationEventPublisher {
@@ -20,5 +22,22 @@ public class MyAuthenticationEventPublisher extends DefaultAuthenticationEventPu
 			System.out.println(oe.getError());
 		}
 		super.publishAuthenticationFailure(exception, authentication);
+		
+	}
+
+	@Override
+	public void publishAuthenticationSuccess(Authentication authentication) {
+		System.out.println(authentication);
+		System.out.println(authentication.getClass().getName());
+		if (authentication instanceof OAuth2LoginAuthenticationToken) {
+			OAuth2LoginAuthenticationToken token = (OAuth2LoginAuthenticationToken) authentication;
+			
+			OAuth2AccessToken accessToken = token.getAccessToken();
+			System.out.println("AccessToken.Value: " + accessToken.getTokenValue());
+			System.out.println("AccessToken.Type: " + accessToken.getTokenType().getValue());
+			System.out.println("AccessToken.Scopes: " + accessToken.getScopes());
+			System.out.println("AccessToken.ExpiresAt: " + accessToken.getExpiresAt());
+		}
+		super.publishAuthenticationSuccess(authentication);
 	}
 }
